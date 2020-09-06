1fb494d970660328761943b14e41e3131a0e405c
Ana Paula <apaula@localhost.localdomain>
2020-09-05 22:52:08 -0400

    Tentativa de ondas

--- web/vip/src/pages/TelaInicial/index.tsx
+++ web/vip/src/pages/TelaInicial/index.tsx
@@ -5,6 +5,7 @@ import Row from "react-bootstrap/Row";
 import { Link } from "react-router-dom";
 import Col from "react-bootstrap/Col";
 import "./styles.css";
+import onda1 from "../../assets/svg/onda1.svg";
 
 function TelaInicial() {
   return (
@@ -24,14 +25,41 @@ function TelaInicial() {
           </Col>
         </Row>
         <Row>
-          <Col>2 of 3</Col>
-          <Col>2 of 3</Col>
+          <Col>
+            <Link to="/logar">Logar</Link>
+          </Col>
+          <Col>
+            <Link to="/cadastrar">Cadastrar</Link>
+          </Col>
         </Row>
       </Container>
-      <p>Teste</p>
-      <a href="/logar">logar</a>
-      <br></br>
-      <a href="/cadastrar">cadastrar</a>
+      <div className="footer">
+        <svg
+          className="waves"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 622 385"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M-13.3239 116.355C-18.2394 72.6019 -18.7688 30.4553 -17.2423 0L-13.3239 116.355C-4.00534 199.299 21.0761 288.015 77.7958 313.794C186.095 363.017 278.923 354.813 362.91 371.221C431.941 384.707 511.424 441.144 576.814 456.261C614.059 456.783 631.197 457.774 617.082 459.411C604.487 460.872 590.982 459.537 576.814 456.261C464.767 454.69 170.744 457.359 -1.77095 459.411L-13.3239 116.355Z"
+            fill="url(#paint0_linear)"
+          />
+          <defs>
+            <linearGradient
+              id="paint0_linear"
+              x1="146.312"
+              y1="344.559"
+              x2="19.1327"
+              y2="450.157"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stop-color="#6F77A6" />
+              <stop offset="1" stop-color="#3B568C" />
+            </linearGradient>
+          </defs>
+        </svg>
+      </div>
     </div>
   );
 }
